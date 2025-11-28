e63b82e14683d94fd8386b6a8c29de174d471646
Kukaracka <kukaracka@inbox.ru>
2025-11-28 18:16:08 +0700

    docs(repo): autopomplete suggestions improved

--- backend/src/repositories/user_repository.py
+++ backend/src/repositories/user_repository.py
@@ -1,13 +1,16 @@
+from typing import Optional
 from sqlalchemy import select
-from database.models import UserModel
+from database.models import UserModel, Session
 from utils.repository import SQLAlchemyRepository
 
 
 class UserRepository(SQLAlchemyRepository[UserModel]):
-    model = UserModel
+    def __init__(self):
+        super().__init__(UserModel)
 
-    async def get_by_username(self, username: str) -> UserModel | None:
-        async with self.SessionLocal() as session:
+    async def get_by_username(self, username: str) -> Optional[UserModel]:
+        """Получить пользователя по username"""
+        async with Session() as session:
             statement = select(UserModel).where(UserModel.username == username)
             result = await session.execute(statement=statement)
             return result.scalar_one_or_none()
